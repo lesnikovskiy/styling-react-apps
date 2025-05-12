311d4f1b831c760674f8582f23b20db0f7b8527b
lesnikovski <lesnikovski@gmail.com>
2025-05-12 19:15:53 +0300

    Style react apps

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
+import Button from './Button.jsx';
 
 const ControlContainer = styled.div`
   display: flex;
@@ -30,21 +31,6 @@ const Input = styled.input`
 
   &:focus {
     outline: none;
-  }
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
   }
 `;
 
