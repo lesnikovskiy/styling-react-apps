6987e82a23c56c961977a798bfad74a940f13cad
lesnikovski <lesnikovski@gmail.com>
2025-09-14 22:15:14 +0300

    Complete tailwind css migration

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,27 +1,6 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 import Button from './Button.jsx';
 import Input from './Input.jsx';
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
-const TextButton = styled.button`
-  border: none;
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  color: #f0b322;
-
-  &:hover {
-    color: #f0920e;
-    outline: none;
-  }
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -44,8 +23,11 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <Input
           type="email"
           label="Email"
@@ -55,14 +37,16 @@ export default function AuthInputs() {
         <Input
           type="password"
           label="Password"
-          $invalid={passwordNotValid}
+          invalid={passwordNotValid}
           onChange={(event) =>
             handleInputChange('password', event.target.value)
           }
         />
-      </ControlContainer>
-      <div className="actions">
-        <TextButton type="button">Create a new account</TextButton>
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
+          Create a new account
+        </button>
         <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
